c3b25aa563200acf6c80d75a650bf354608fc8ed
Evert Pot <me@evertpot.com>
2022-04-18 23:10:51 -0400

    space

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { OAuth2Client } from './client';
 export { OAuth2Fetch } from './fetch-wrapper';
 export { OAuth2Token } from './token';
-export { OAuth2Error }from './error';
+export { OAuth2Error } from './error';
