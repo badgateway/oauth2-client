af7d56bfbbb599c2f2df8994e2c59016a327d36f
Evert Pot <me@evertpot.com>
2022-05-10 02:02:25 -0400

    show me the logs

--- test/pkce.ts
+++ test/pkce.ts
@@ -6,6 +6,7 @@ describe('generateCodeVerifier', () => {
   it('should generate a 32byte base4url string', async () => {
 
     const out = await generateCodeVerifier();
+    console.debug(out, out.length);
     expect(out).to.match(/^[A-Za-z0-9-_]{43}$/);
 
   });
