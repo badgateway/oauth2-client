8f421835723ec2a58e201ce70fd1e58fa23f667d
Evert Pot <me@evertpot.com>
2022-07-22 13:14:46 -0400

    Testing PKCE code challenge generating on Node

--- test/pkce.ts
+++ test/pkce.ts
@@ -6,7 +6,7 @@ describe('generateCodeVerifier', () => {
   it('should generate a 32byte base4url string', async () => {
 
     const out = await generateCodeVerifier();
-    console.debug(out, out.length);
+    //console.debug(out, out.length);
     expect(out).to.match(/^[A-Za-z0-9-_]{43}$/);
 
   });
